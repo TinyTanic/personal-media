200835497b0cb3c3be2f2fd5bafe816eaa890495
Luca Aguzzoli <luca.aguzzoli@edu.unito.it>
2016-02-05 21:27:23 +0100

    added the dinamic creation of the film page

--- app/film/index.js
+++ app/film/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
-
+var films = {
+   films: [{
+      title: 'avatar',
+      genre: 'fantascienza',
+      image: 'images/avatar.jpg'
+   }, {
+      title: 'altro film',
+      genre: 'commedia',
+      image: 'images/avatar.jpg'
+   }]
+};
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('film/film');
+   res.render('film/film', films);
 });
 
 module.exports = router;
